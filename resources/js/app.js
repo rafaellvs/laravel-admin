e11dc63954dd6bcce908a1e5912a98f9f8e73bcd
Rafael Vieira <rafaelv@lapisraro.com.br>
2019-07-10 19:38:47 -0300

    boilerplate

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+require('./bootstrap');
+
+$(document).ready(function(){
+    $('.login').css('height', $(window).height() * 0.8)
+})
+
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::post('/', function(Request $request) {
+    return view('jansen', [
+        'request' => $request->all()
+    ]);
+});
